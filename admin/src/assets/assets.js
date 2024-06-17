8db53f7f60666da41c64e7979c80338d2b89ba90
theamantaneja <theamantaneja@gmail.com>
2024-06-17 21:50:51 +0530

    updates

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
 
-export const url = 'http://localhost:4000'
+export const url = 'https://rasoionwheels.onrender.com'
 export const currency = '₹'
 
 export const assets ={
